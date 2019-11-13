4daab16a393f2249de6a6f6899c58ecd96fce93a
Pando85 <pando855@gmail.com>
2019-11-13 20:14:30 +0100

    Add version to forwarder package and fix tag_version.sh script with forwarder package file names

--- .ci/tag_version.sh
+++ .ci/tag_version.sh
@@ -2,20 +2,17 @@
 set -e
 
 check_version_changes(){
-    git diff  $(git describe --tags --abbrev=0 HEAD)..HEAD -- aiolambda/__init__.py | grep --quiet +__version__;
+    git diff  $(git describe --tags --abbrev=0 HEAD)..HEAD -- forwarder/__init__.py | grep --quiet +__version__;
 };
 
 if ! check_version_changes; then
     echo "Not version changed"
     exit
 fi
-VERSION=$(python -c 'import aiolambda; print(aiolambda.__version__)')
+VERSION=$(python -c 'import forwarder; print(forwarder.__version__)')
 
 if [ ! -z "${TRAVIS_REPO_SLUG}" ]; then
     git tag -a $VERSION -m "version $VERSION"
     git push "https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG" $VERSION
 fi
 
-WORKDIR=$(dirname $(realpath $0))
-
-bash $WORKDIR/make_changelog.sh
