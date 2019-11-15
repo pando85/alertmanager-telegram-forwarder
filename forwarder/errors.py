a5a6f0e6424a42789f29bbbcc2c1226a5071fe59
Pando85 <pando855@gmail.com>
2019-11-15 20:52:12 +0100

    - add send_alerts to handlers - change mock telegram response error - remove alerts handler test

--- forwarder/errors.py
+++ forwarder/errors.py
@@ -1,6 +1,8 @@
+from forwarder.types import Json
+
 
 class ResponseError(Exception):
-    def __init__(self, status_code: int, message: str):
+    def __init__(self, status_code: int, message: Json):
         super().__init__(status_code, message)
         self.status_code = status_code
         self.message = message
